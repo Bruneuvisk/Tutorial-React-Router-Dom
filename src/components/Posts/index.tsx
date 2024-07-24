9eac9cf3091526d3250d9e645d4713f24d43c0d4
Brunao544 <bruneuvisk525@outlook.com>
2024-07-24 19:39:48 -0300

    Tutorial React Router Dom

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Posts = () => {
+    return (
+        <div>
+            <h1>Post</h1>
+            <p>Proident adipisicing do cillum culpa dolore. Amet et ullamco consequat sit amet ut nulla quis veniam mollit. Ad duis ea in ex Lorem do. Deserunt cillum aliqua enim Lorem nostrud cupidatat ut exercitation occaecat ut laborum fugiat.</p>
+        </div>
+    );
+};
